91f8606562c6c552a8a54ab2c9576b213152d414
Vlad Ermachenkov <ermachenkov.v.s@gmail.com>
2020-02-16 21:54:27 +0300

    Updated debug build config

--- build_options.cmake
+++ build_options.cmake
@@ -2,16 +2,19 @@
 #Need to clarify how this feature works
 # set(LTO_OPT "-flto")
 ###############################################################################
+set(CMAKE_BUILD_TYPE_INIT Debug)
+
+message("Current build type is ${CMAKE_BUILD_TYPE}")
 if (NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE Debug CACHE STRING "Default build type is ${CMAKE_BUILD_TYPE}" FORCE)
 	message("Default build type is ${CMAKE_BUILD_TYPE}")
-endif(NOT CMAKE_BUILD_TYPE) 
+endif() 
 
 ###############################################################################
-set(CMAKE_C_FLAGS_DEBUG "-O3 -g3 ${LTO_OPT}" CACHE INTERNAL "C Compiler options for debug build" FORCE)
-set(CMAKE_CXX_FLAGS_DEBUG "-O3 -g3 ${LTO_OPT}" CACHE INTERNAL "C++ Compiler options for debug build" FORCE)
+set(CMAKE_C_FLAGS_DEBUG "-Og -g3 ${LTO_OPT}" CACHE INTERNAL "C Compiler options for debug build" FORCE)
+set(CMAKE_CXX_FLAGS_DEBUG "-Og -g3 ${LTO_OPT}" CACHE INTERNAL "C++ Compiler options for debug build" FORCE)
 set(CMAKE_ASM_FLAGS_DEBUG "-g3" CACHE INTERNAL "ASM Compiler options for debug build" FORCE)
-set(CMAKE_EXE_LINKER_FLAGS_DEBUG "-O3 -g3 ${LTO_OPT}" CACHE INTERNAL "Linker options for debug build" FORCE)
+set(CMAKE_EXE_LINKER_FLAGS_DEBUG "-Og -g3 ${LTO_OPT}" CACHE INTERNAL "Linker options for debug build" FORCE)
 ###############################################################################
 set(CMAKE_C_FLAGS_RELEASE "-O3 ${LTO_OPT}" CACHE INTERNAL "C Compiler options for release build" FORCE)
 set(CMAKE_CXX_FLAGS_RELEASE "-O3 ${LTO_OPT}" CACHE INTERNAL "C++ Compiler options for release build" FORCE)
